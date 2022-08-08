d960a277dd42a86a7d3571f9d83d904f8606c248
Henry Mai <henrymai@users.noreply.github.com>
2022-08-08 05:27:03 +0000

    Very rough skeleton for BlockUtilsPropertiesTest

--- athena-federation-sdk/src/test/java/com/amazonaws/athena/connector/lambda/data/BlockUtilsPropertiesTest.java
+++ athena-federation-sdk/src/test/java/com/amazonaws/athena/connector/lambda/data/BlockUtilsPropertiesTest.java
@@ -19,19 +19,95 @@ package com.amazonaws.athena.connector.lambda.data;
  * limitations under the License.
  * #L%
  */
-
 import net.jqwik.api.*;
 import org.assertj.core.api.*;
 
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.Schema;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.memory.RootAllocator;
+
+import com.amazonaws.athena.connector.lambda.data.FieldResolver;
+import com.amazonaws.athena.connector.lambda.data.BlockUtils;
+
 class BlockUtilsPropertiesTest {
 
-  @Property
-  boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-      int absVal = Math.abs(anInteger);
-      if (anInteger == Integer.MIN_VALUE) {
-          return absVal == Integer.MIN_VALUE;
-      }
-      return absVal >= 0;
+  // Do not actually implement/generate this class (`FieldAndData`).
+  //
+  // Just using this to kind of organize the thought process.
+  //
+  // Also read `setComplexValuesSetsAllFieldsCorrectlyGivenAnyInput` first to get an idea of
+  // what we want to generate and what the test is checking.
+  //
+  // ***
+  // The main idea is that we have an overarching `Arbitrary` for `VectorSchemaRoot`; lets refer to
+  // this as `VectorSchemaRootArbitrary`.
+  // Inside `VectorSchemaRootArbitrary`, it will use an `Arbitrary` for Field (that you implement) to generate a `Field`.
+  // Then given that generated `Field`, the `VectorSchemaRootArbitrary` uses `Arbitrary`s for `___Vector`s that correspond to
+  // the Field's children Fields (this will have to be depth first recursive).
+  // ***
+  class FieldAndData {
+      // Two options here:
+      //
+      // A)
+      // Write an `Arbitrary` that generates a glue string type recursively (which means
+      // you'll need `Arbitrary` generators for the primitive glue type strings first).
+      // Then just use GlueFieldLexer.lex to convert that into an Arrow Field.
+      // Note that we will have to eventually also test GlueFieldLexer seperately, but for now
+      // its ok to pretend that we can rely on it.
+      //
+      // public String glueSchema;
+      //
+      // or
+      //
+      // B)
+      // Write an `Arbitrary` to generate the Field directly and recursively.
+      // You will need to write arbitraries for all the different kinds of `ArrowType`s that a FieldType can encapsulate.
+      // See this for reference on how to construct a Field directly:
+      //    https://arrow.apache.org/docs/java/quickstartguide.html#create-a-field
+      public Field field;
+
+      // Schema should just single item list containing the field generated above, like: `new Schema(List.of(field))`
+      // Note that setComplexValue only operates on a single FieldVector, so we'll always only have a single Field in the Schema.
+      public Schema schema;
+
+      // Using that Field schema above, generate an arrow object (VectorSchemaRoot)
+      // The inner FieldVector is what will be passed into setComplexValue as `Object value`.
+      // See this for how to set the values in it:
+      //    https://arrow.apache.org/docs/java/quickstartguide.html#create-a-vectorschemaroot
+      public VectorSchemaRoot schemaRoot;
+
+      // For the `FieldResolver resolver` parameter to `setComplexValue`:
+      // Need to write a custom FieldResolver that just extracts Arrow values.
+      // The idea is that a FieldResolver going from Arrow to Arrow should be simple and
+      // that we aren't testing the FieldResolver, but rather if setComplexValue will
+      // set the values correctly in for sorts of complicated glueSchemas and example values
+      // generated off of that schema.
+
+      // See this documentation from jqwik on how to build more complicated arbitraries from simpler ones here:
+      //    https://jqwik.net/docs/current/user-guide.html#combining-arbitraries
+      // This section on recursive arbitraries should also be useful:
+      //    https://jqwik.net/docs/current/user-guide.html#recursive-arbitraries
   }
 
+  // setComplexValue(FieldVector vector, int pos, FieldResolver resolver, Object value)
+  @Property
+  boolean setComplexValuesSetsAllFieldsCorrectlyGivenAnyInput(@ForAll VectorSchemaRoot inputSchemaRoot) {
+      // Create an empty `outputSchemaRoot` so that we can pass in its internal FieldVector to be written to by setComplexValue.
+      VectorSchemaRoot outputSchemaRoot = VectorSchemaRoot.create(inputSchemaRoot.getSchema(), new RootAllocator());
+      BlockUtils.setComplexValue(outputSchemaRoot.getVector(0), 0 /*I think*/, new ArrowToArrowResolver(), inputSchemaRoot.getVector(0));
+      return outputSchemaRoot.equals(inputSchemaRoot);
+  }
+
+}
+
+
+class ArrowToArrowResolver
+    implements FieldResolver
+{
+    @Override
+    public Object getFieldValue(Field field, Object originalValue) {
+        // TODO: Implement
+        return null;
+    }
 }
